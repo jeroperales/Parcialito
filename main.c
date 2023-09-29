7754e7cffd50ee0fe71729d96ee5f86d34a13651
jeroperales <134175102+jeroperales@users.noreply.github.com>
2023-09-28 21:08:47 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct stNodo
+{
+    int num;
+    struct stNodo * siguiente;
+
+} stNodo;
+
+
+//PROTOTIPADO
+stNodo * inicLista();
+stNodo * crearNodo(int num);
+stNodo * cargarListaRand(stNodo * lista);
+stNodo * agregarUltimo (stNodo * lista, stNodo * nuevoNodo);
+stNodo * buscarUltimo(stNodo * lista);
+void mostrarLista(stNodo * lista);
+void mostrarListaRecursivo(stNodo * lista);
+
+int main()
+{
+    //PUNTO 1 Crear una lista enlazada de números enteros positivos al azar, la inserción se realiza por el último nodo.
+    stNodo * lista = inicLista();
+    lista = cargarListaRand(lista);
+    printf("DE IZQUIERDA A DERECHA \n");
+    mostrarLista(lista);
+
+
+    //PUNTO 2
+    printf("\nDE DERECHA A IZQUIERDA \n");
+    mostrarListaRecursivo(lista);
+
+
+    return 0;
+}
+
+stNodo * inicLista()
+{
+    return NULL;
+}
+
+stNodo * crearNodo(int num)
+{
+    stNodo * aux = (stNodo*)malloc(sizeof(stNodo));
+    aux->num = num;
+    aux->siguiente = NULL;
+    return aux;
+}
+
+
+
+stNodo * cargarListaRand(stNodo * lista)
+{
+    stNodo * aux = lista;
+    stNodo * nuevoNodo;
+    int aux1, aux2, i;
+
+    printf("CUANTOS NUMEROS DESEAN QUE SE CARGUE\n");
+    scanf("%i", &aux1);
+
+    srand(time(NULL));
+
+
+    for (i=0; i<aux1; i++)
+    {
+        aux2 = rand() % 100;
+        nuevoNodo = crearNodo(aux2);
+        aux = agregarUltimo(aux, nuevoNodo);
+
+    }
+
+    return aux;
+}
+
+stNodo * agregarUltimo (stNodo * lista, stNodo * nuevoNodo)
+{
+    stNodo * aux = lista;
+    stNodo * ultimo;
+
+    if(lista != NULL)
+    {
+        ultimo = buscarUltimo(aux);
+        ultimo->siguiente = nuevoNodo;
+    }
+    else
+    {
+        aux = nuevoNodo;
+    }
+
+    return aux;
+}
+
+stNodo * buscarUltimo(stNodo * lista)
+{
+    stNodo * seg = lista;
+    if(seg != NULL)
+        while(seg->siguiente != NULL)
+        {
+            seg = seg->siguiente;
+        }
+    return seg;
+}
+
+
+void mostrarLista(stNodo * lista)
+{
+    stNodo * aux = lista;
+    while (aux !=NULL)
+    {
+        printf("| %i | ", aux->num);
+        aux=aux->siguiente;
+    }
+}
+
+void mostrarListaRecursivo(stNodo * lista)
+{
+    stNodo * aux = lista;
+
+    if (aux != NULL)
+    {
+        mostrarListaRecursivo(aux->siguiente);
+        printf("| %i | ", aux->num);
+
+        }
+
+}
