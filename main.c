d5f914871bb2bb4407a1ca6e770e4166199b00ec
jeroperales <134175102+jeroperales@users.noreply.github.com>
2023-09-30 21:22:18 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 
 
+//ESTRUCTURAS NODOS
+
 typedef struct stNodo
 {
     int num;
     struct stNodo * siguiente;
 
 } stNodo;
+
+typedef struct stNodoDoble
+{
+    int num;
+    struct stNodoDoble * siguiente;
+    struct stNodoDoble * anterior;
+} stNodoDoble;
+
+
 
 
 //PROTOTIPADO
@@ -20,13 +31,24 @@ void mostrarLista(stNodo * lista);
 void mostrarListaRecursivo(stNodo * lista);
 int contaNodos(stNodo * lista);
 stNodo * borrarNodoPosi(stNodo * lista, int posi);
+stNodo * borraNodosMayores (stNodo * lista, int limite);
+/*
+stNodoDoble * crearNodoDoble(int num);
+stNodoDoble * agregarPrincipioDoble (stNodoDoble * lista, stNodoDoble * nuevo);
+stNodoDoble * pasarEnOrden(stNodoDoble * listaDoble, stNodo * lista);
+void mostrarListaDoble(stNodoDoble * lista);
+*/
 
 int main()
 {
     //VARIABLES
     int validos;
     int posi;
+    int limite;
     int loop = 0;
+    int opc;
+
+
 
     //PUNTO 1 Crear una lista enlazada de numeros enteros positivos al azar, la insercion se realiza por el ultimo nodo.
     stNodo * lista = inicLista();
@@ -43,26 +65,57 @@ int main()
     validos = contaNodos(lista);
     printf("\n\nCANTIDAD DE NODOS GENERADOS: %i\n", validos);
 
-    //PUNTO 4
-    while (loop == 0)
+
+    printf("\n\n");
+    printf("================MENU================\n");
+    printf(" 1- Eliminar segun POSICION\n");
+    printf(" 2- Eliminar todo numero MAYOR a numero ingresado\n");
+    printf(" 3- Pasar de lista SIMPLE a lista DOBLE\n");
+    printf("====================================");
+    fflush(stdin);
+    printf("\nIngrese una opcion: \n");
+    scanf("%i", &opc);
+
+    switch(opc)
     {
-        printf("\nINGRESE LA POSICION A ELIMINAR\n");
-        scanf("%i", &posi);
 
-        if (posi <= validos && posi != 0)
-        {
-            lista = borrarNodoPosi(lista, posi);
-            loop = 1;
-        }
-        else
-        {
-            printf("\n\nPOSICION INVALIDA, INGRESE NUEVAMENTE \n\n");
+    case 1:
 
+        //PUNTO 4
+        while (loop == 0)
+        {
+            printf("\nINGRESE LA POSICION A ELIMINAR\n");
+            scanf("%i", &posi);
+
+            if (posi <= validos && posi != 0)
+            {
+                lista = borrarNodoPosi(lista, posi);
+                loop = 1;
+            }
+            else
+            {
+                printf("\n\nPOSICION INVALIDA, INGRESE NUEVAMENTE \n\n");
+
+            }
         }
+        printf("LISTA CON DICHO NODO ELIMINADO\n");
+        mostrarLista(lista);
+        break;
+
+    case 2:
+        printf("INGRESE A PARTIR DE QUE NUMERO DESEA BORRAR\n");
+        scanf("%i", &limite);
+        lista = borraNodosMayores(lista, limite);
+        mostrarLista(lista);
+        break;
+
+    case 3:
+        /*listaDoble = pasarEnOrden(listaDoble, lista);
+        mostrarListaDoble (listaDoble);*/
+        break;
+
+
     }
-    printf("LISTA CON DICHO NODO ELIMINADO\n");
-    mostrarLista(lista);
-
     return 0;
 }
 
@@ -207,3 +260,100 @@ stNodo * borrarNodoPosi(stNodo * lista, int posi)
 
     return lista;
 }
+
+
+stNodo * borraNodosMayores (stNodo * lista, int limite)
+{
+    stNodo * aux = lista;
+    stNodo * ante =NULL;
+
+    while (aux != NULL)
+    {
+        if (aux->num <= limite)
+        {
+            ante = aux;
+            aux=aux->siguiente;
+        }
+        else
+        {
+            if (ante == NULL)
+            {
+                lista = aux->siguiente;
+                free(aux);
+                aux = lista;
+            }
+            else
+            {
+                ante->siguiente = aux->siguiente;
+                free(aux);
+                aux = ante->siguiente;
+            }
+
+
+        }
+    }
+
+    return lista;
+}
+
+/*
+stNodoDoble* crearNodoDoble(int num)
+{
+    stNodoDoble* aux = (stNodoDoble*)malloc(sizeof(stNodoDoble));
+    aux->num = num;
+    aux->anterior = NULL;
+    aux->siguiente = NULL;
+
+    return aux;
+}
+
+stNodoDoble * agregarPrincipioDoble (stNodoDoble * lista, stNodoDoble * nuevo) {
+    nuevo->siguiente = lista;
+    if(lista != NULL)
+       lista->anterior=nuevo;
+
+    return nuevo;
+}
+
+
+
+stNodoDoble* pasarEnOrden(stNodoDoble * listaDoble, stNodo * lista)
+{
+    stNodoDoble * auxDoble = listaDoble;
+    stNodoDoble * nuevoNodo;
+    stNodo * aux = lista;
+
+
+
+
+        if(aux->num <= auxDoble->num)
+        {
+
+            nuevoNodo = crearNodoDoble (aux->num);
+            auxDoble = agregarPrincipioDoble(auxDoble, nuevoNodo);
+
+        }
+        else
+        {
+            printf("B");
+            auxDoble->siguiente = aux;
+            aux = aux->siguiente;
+
+        }
+    printf("HOLA");
+        return auxDoble;
+
+
+}
+
+void mostrarListaDoble(stNodoDoble * lista)
+{
+    stNodoDoble * aux = lista;
+    while (aux !=NULL)
+    {
+        printf("| %i | ", aux->num);
+        aux=aux->siguiente;
+    }
+}
+*/
+
